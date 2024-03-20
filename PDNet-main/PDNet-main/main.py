071d39208cc80dd9f3038c17e90d7825483354fb
mengyuanze <1977245719@qq.com>
2024-03-20 18:49:00 +0800

    fix

--- PDNet-main/PDNet-main/main.py
+++ PDNet-main/PDNet-main/main.py
@@ -3,8 +3,9 @@ from ultralytics import YOLO
 
 if __name__ == '__main__':
     model = YOLO("ultralytics/cfg/models/v8/yolov8PDNet.yaml")
-    results = model.train(data='', epochs=200)
+    results = model.train(data='datasets/datavoc.yaml', epochs=200)
     results = model.train(resume=True)
+
 
 
 
